b0dc5c92bfe8d326feb101b806fb799da0565426
André <a.silva14@hotmail.com>
2020-09-16 15:07:13 +0200

    FIesta Boom Boom

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
-#include <windows.h>
+#include <iostream>
 
-int main()
+int main ()
 {
-	MessageBox(nullptr, "World!", "Hello", MB_ICONEXCLAMATION);
+	std::cout << " Roll your dice between 1 and 8 to beat Boss\n";
+
+	int pvFinaux = 100;
+	int value = 0 ;
+	std::cin >> value;
+	int attaque = 6;
+	int defense = 30;
+	int attaqueFinale = attaque * value;
+	
+
+	bool exit = false;
+	
+	while ( !exit )
+	{
+		
+		if (attaqueFinale > defense)
+		{
+			std::cout << "B perd des pv\n";
+			pvFinaux = pvFinaux- (attaqueFinale - defense );
+		}
+		else
+		{
+			std::cout << "Esquive\n";
+		}
+		std::cout << pvFinaux << std::endl;
+		if (pvFinaux <= 0)
+		{
+			break;
+		}
+		system("pause");
+	}
+	
+
 }
