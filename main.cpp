3a093b7ac79ef579ed6ec90739cf4c00e2de7418
André <a.silva14@hotmail.com>
2020-09-21 15:58:49 +0200

    First

--- main.cpp
+++ main.cpp
@@ -1,38 +1,100 @@
 #include <iostream>
-
-int main ()
+#include <chrono>
+#include <Windows.h>
+#include <string>
+#include <string_view>
+void where()
 {
-	std::cout << " Roll your dice between 1 and 8 to beat Boss\n";
-
-	int pvFinaux = 100;
-	int value = 0 ;
-	std::cin >> value;
-	int attaque = 6;
-	int defense = 30;
-	int attaqueFinale = attaque * value;
-	
-
-	bool exit = false;
-	
-	while ( !exit )
+	std::cout << "Bathroom\n";
+}
+void move()
+{
+	std::cout << "Where do you wanna go ?";
+}
+void Bathroom()
+{
+	std::cout << "Wanna take a bath ? kidding, pick up that bat.\n";
+	std::cout << "Pick it up ? Go Back ?";
+}
+void Rat()
+{
+	std::cout << "A wild rat appears !\n What are you gonna do ?\n Attack ? Run ?";
+}
+void exit()
+{
+	return;
+}
+void Attack()
+{
+	int PVrat = 10;
+	bool attacking = true;
+	std::cout << "You're fighting the RAT !\n What do we do now ?\n Hit ?\n Defense ?(You can only hit honestly)\n";
+	while (attacking)
 	{
-		
-		if (attaqueFinale > defense)
+		std::string choice;
+		std::getline(std::cin, choice);
+		if (choice == "hit")
 		{
-			std::cout << "B perd des pv\n";
-			pvFinaux = pvFinaux- (attaqueFinale - defense );
-		}
-		else
+			PVrat = PVrat - 2;
+			std::cout << PVrat << "\nPV\n";		}	
+		if (PVrat <= 0)
 		{
-			std::cout << "Esquive\n";
-		}
-		std::cout << pvFinaux << std::endl;
-		if (pvFinaux <= 0)
+			attacking = false;
+			std::cout <<  "You've just won nothing congrats\n ! Write exit to quit\n Or write again to play again\n";
+		}	
+	}
+}
+int main()
+{
+	bool playing = true;
+	std::cout << "Hey !\n";
+	std::cout << " Enter username ";
+	std::string Username;
+	std::getline(std::cin, Username);
+	std::cout << "Welcome ! " << Username;
+	std::cout << " What you wanna do ?\n You are alone at home, your parents went out for the week end but are in their way home.\n You gotta take care of that Rat in the bathroom before they come back.\n Go there ? (Yes/NO)\n";
+	while (playing)
+	{
+		std::string value;
+		std::getline(std::cin, value);
+		if (value == "again")
 		{
-			break;
+			main();
 		}
-		system("pause");
+		if (value == "Run") {
+
+			playing = false;
+			std::cout << "You failed Loser.";
+		}
+		if (value == "Go back") {
+
+			playing = false;
+			std::cout << "You failed Loser.\n The Rat lived and killed your parents.";
+		}
+		if (value == "No") {
+			
+			playing = false;
+			std::cout << "You failed Loser.";
+		}
+		if (value == "Yes") {
+			Bathroom();
+		}
+		if (value == "where") {
+			where();
+		}
+		if (value == "Pick it up")
+		{
+			Rat();
+		}
+		if (value == "Attack")
+		{
+			Attack();// système d'attaque et de defense ( points de vie, randomizer etc..)
+		}
+		if (value == "exit") {
+			MessageBox(nullptr, "around !", "See you", MB_ICONEXCLAMATION);
+			//std::cout << "See you around.";
+			return 0;	
+		}
 	}
 	
-
 }
